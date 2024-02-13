79f3937bb94e625cbd35f2544bcae45acd8089aa
Sateesh <sateesh.jh@gmail.com>
2024-02-13 23:50:58 +0400

    Added the DB model classes

---
+++ app/src/main/java/com/sateeshjh/realmdbguide/models/Address.kt
@@ -0,0 +1,17 @@
+package com.sateeshjh.realmdbguide.models
+
+import io.realm.kotlin.types.EmbeddedRealmObject
+
+// DB Relations
+// Teacher 1-to-1 Address
+// Teacher 1-to-many Course
+// Students many-to-many Course
+
+class Address: EmbeddedRealmObject  {
+    var fullName: String = ""
+    var street: String = ""
+    var houseNumber: Int = 0
+    var zip: Int = 0
+    var city: String = ""
+    var teacher: Teacher? = null
+}
